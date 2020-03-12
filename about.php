c0375e92276c626fe7883cde9e8c11093a0f64df
ilhamfannani <47444329+ilhamfannani@users.noreply.github.com>
2020-03-12 22:58:42 +0700

    Penambahan Fitur Dalam Aplikasi Point Of Sales
    On Progress, Perlu Revisi Lagi

---
+++ about.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<style type="text/css">
+		.about{
+			margin-top: 10%;
+		}.container h3{
+			color: #696969;
+		}.container h4{
+color: #696969;
+            
+		}.about img{
+			width: 200px;
+		}.team1 h4{
+			text-align: right;
+		}.team1 p{
+			text-align: right;
+		}.team1 {
+margin-top: 5%;
+		}.team2 {
+margin-top: 5%;
+		}.team3{
+margin-top: 5%;
+text-align: right;
+			}.team4{
+			margin-top: 5%;
+			padding-bottom: 10%;	
+			}.footer{
+            background-color: #2F4F4F;
+            padding-top: 5%;
+            color: #ffffff;
+            }iframe{
+               
+            }.footer hr{
+                background-color: #FF6347;
+                width: 50%;
+            }.map h3{
+                color: #696969;
+            }
+	</style>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<title>About</title>
+</head>
+<body>
+ <?php include 'nav.php' ?>
+
+ <div class="container">
+ 	<div class="about">
+ 		<div class="row">
+ 			<div class="col-lg-12">
+ 				<h3 class="h3">About Us</h3>
+ 				<hr>
+ 				<p>Sewa Bus Kuy merupakan perusahaan di bidang pelayanan jasa transportasi daerah bandung, dimana transportasi yang disediakan seperti Bus atau travel, 
+ 				Sewa Bus kuy menyediakan sistem pembookingan layanan lewat aplikasi berbasis website yang diman prosesnya cepat dan terpercaya.</p>
+ 			</div>
+ 		</div>
+ 		<div class="row">
+ 			<div class="col-lg-12">
+ 				<h3>Team Management</h3>
+ 				<hr>
+ 			</div>
+ 		</div>
+ 		<div class="row team">
+ 			<div class="col-lg-3">
+ 				<img class="rounded-circle" src="img/ilhamm.jpg" width="30px">
+ 			</div>
+ 			<div class="col-lg-9">
+ 				<h4>Ilham Fanani</h4>
+ 				<p>Ilham Fanani menjadi <b>PROJECT MANAGER</b> yang bertugas Memastikan semua rencana project sudah tersusun dan berjalan sesuai dengan waktu yang di tentukan  <p>
+ 			</div>
+ 		</div>
+<hr>
+ 		<div class="row team1">
+ 			<div class="col-lg-9">
+ 				<h4 class="team1">Leonardo Taufan</h4>
+ 				<p>Leonardo Taufan menjadi programmer <b>BACK END WEB DEVELOPER</b> yang bertugas membangun dan merancang sistem aplikasi yang telah di tentukan team proyek</p>
+ 			</div>
+ 			<div class="col-lg-3">
+ 				<img class="rounded-circle" src="img/leo.jpg">
+ 			</div>
+ 		</div>
+ 		<p>
+ 			<hr>
+ 			<div class="row team2">
+ 			<div class="col-lg-3">
+ 			<img class="rounded-circle" src="img/agungs.jpg">
+ 			</div>
+ 			<div class="col-lg-9">
+ 				<h4>Agung Riansyah</h4>
+ 				<p>Agung Riansyah menjadi <b>SYSTEM ANALYST</b> yang bertugas Menganalisis kebutuhan dari aplikasi yang akan dibuat  </p>
+ 			</div>
+ 		</div>
+ 		<p>
+ 			<hr>
+ 			<div class="row team3">
+ 			<div class="col-lg-9">
+ 				<h4 class="team1">Taufiq Dirga</h4>
+ 				<p>Taufiq Dirga menjadi <b>DESIGNER</b> Membuat design dan fitur apa saja yang ada di dalam aplikasi yang akan dibuat  
+</p>
+ 			</div>
+ 			<div class="col-lg-3">
+ 				<img class="rounded-circle" src="img/dirgaa.jpg">
+ 			</div>
+ 		</div>
+ 		<p>
+ 			<hr>
+ 		<div class="row team4">
+ 			<div class="col-lg-3">
+ 				<img class="rounded-circle" src="img/ridwann.jpg">
+ 			</div>
+ 			<div class="col-lg-9">
+ 				<h4 class="team1">Muhammad Ridwan</h4>
+ 				<p>Muhammad Ridwan menjadi programmer <b>FRONT END WEB DEVELOPER</b> yang bertugas membangun UI/UX tampilan system aplikasi yang telah di tentukan</p>
+ 			</div>
+ 			
+ 		</div>
+ 	</div>
+ </div>
+ <hr>
+ <center>
+    <div class="container-kartu">
+<div class="container costumer">
+    <div class="row">
+        <div class="col-lg-12">
+            <h3>What Do Our Customers Say?</h3>
+            <hr>
+        </div>
+    </div>
+        <div class="row">
+        <div class="col-lg-4">
+            <div class="card shadow-sm p-3 mb-5 bg-white rounded " style="width: 18rem; height: 19rem;">
+  <img class="card-img-top" src="img/leo.png" alt="Card image cap"style="width: 12rem; height: 10rem;">
+  <div class="card-body">
+    <p class="card-text">I don’t know of another website as comprehensive as this one. They do a really good job.</p>
+  </div>
+</div>
+        </div>
+         <div class="col-lg-4">
+            <div class="card shadow-sm p-3 mb-5 bg-white rounded" style="width: 18rem; height: 19rem;"">
+  <img class="card-img-top" src="img/agung.png" alt="Card image cap" style="width: 12rem; height: 10rem;">
+  <div class="card-body">
+    <p class="card-text">A recommended application for bus travels in Bandung, and  I like to travel comfortably</p>
+  </div>
+</div>
+
+    </div>
+     <div class="col-lg-4">
+            <div class="card shadow-sm p-3 mb-5 bg-white rounded" style="width: 18rem; height: 19rem;"">
+  <img class="card-img-top" src="img/ilham.png" alt="Card image cap" style="width: 12rem; height: 10rem;">
+  <div class="card-body">
+    <p class="card-text">Sewa Bus Kuy leaves room for spontaneity and last minute travel changes.</p>
+  </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</center>
+<hr>
+<center>
+<div class="map">
+    <h3>We Location</h3>
+    <hr>
+    <hr>
+</div>
+</center>
+<center>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.2988673283967!2d107.62834001431776!3d-6.974022670225844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6285c5b7da517%3A0x864485f26a388f95!2sTelkom+University!5e0!3m2!1sid!2sid!4v1543135429023" width="100%" height="380px" frameborder="0" style="border:0" allowfullscreen></iframe>
+    
+</center>
+ <!--foot-->
+ <center>
+<div class="footer">
+    <div class="row">
+        <div class="col-lg-3">
+            <h5>About BusKuy</h5>
+            <hr>
+            <p>
+            <a href="about.php">About Us</a>
+            <p>
+            <a href="Contact.php">Contact Us</a>
+            <p>
+            <a href="Lokas">SiteMap</a>
+        </div>
+        <div class="col-lg-3">
+            <h5>Name Bus</h5>
+            <hr>
+            <p>Bus rinjani</p>
+            <p>Bus train</p>
+            <p>Bus cikampek</p>
+            <p>Bus cijagra</p>
+        </div>
+        <div class="col-lg-3">
+            <h5>Daerah</h5>
+            <hr>
+            <p>Bubat</p>
+            <p>Alun-alun</p>
+            <p>Cijagra</p>
+            <p>Dago</p>
+            <p>Dipati Ukur</p>
+        </div>
+        <div class="col-lg-3">
+            <p>Sewa Bus Kuy merupakan tempat pembookigan Bus di daerah bandung dimana, proses layanan nya cepat dan efektiv,</p>
+             <a href=""><img src="img/ig.png" width="30px"></a>
+            <a href=""><img src="img/fb.png" width="30px"></a>
+            <a href=""><img src="img/wa.png" width="30px"></a>
+
+        </div>
+
+    </div>
+   
+    
+</div>
+</center>
+<hr>
+
+ <?php
+include "chat.php"
+     ?>
+   
+<!--java script-->
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
